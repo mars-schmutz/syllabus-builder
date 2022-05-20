7d25c58e0932c8cf08bb86460e5f81357855dc8a
Marshall Schmutz <darthmars97@gmail.com>
2022-05-20 02:08:42 -0600

    get basic pdf generation/download working

--- client/src/components/DocPrefs.vue
+++ client/src/components/DocPrefs.vue
@@ -5,6 +5,7 @@
             <label>Page numbers</label>
             <input type="checkbox" v-model="usePageNumbers">
         </div>
+        <button @click="downloadPdf">Download PDF</button>
     </div>
 </template>
 
@@ -23,6 +24,26 @@ export default {
             store,
             usePageNumbers,
         }
+    },
+
+    methods: {
+        downloadPdf() {
+            var data = "name=" + encodeURIComponent("mersh");
+
+            fetch("http://localhost:3000/download", {
+                method: "POST",
+                body: data,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function(response) {
+                return response.blob()
+            }).then(function(blob) {
+                const url = window.URL.createObjectURL(blob);
+                window.open(url);
+                window.URL.revokeObjectURL(url);
+            })
+        },
     }
 }
 </script>
--- server/server.js
+++ server/server.js
@@ -1,2 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const pdfkit = require('pdfkit');
+
 const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/download", (req, res) => {
+    const doc = new pdfkit({
+        bufferPages: true,
+        size: 'A7',
+    });
+    let buffers = [];
+
+    doc.on('data', buffers.push.bind(buffers));
+    doc.on('end', () => {
+        let data = Buffer.concat(buffers);
+        res.writeHead(200, {
+            'Content-Length': Buffer.byteLength(data),
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment;filename=syllabus.pdf',
+        }).end(data);
+        console.log("PDF sent");
+    });
+
+    doc.font('Times-Roman')
+        .fontSize(12)
+        .text(`Hello, ${req.body.name}!`, 100, 100);
+    doc.end();
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
